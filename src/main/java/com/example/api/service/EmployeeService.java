a46e7805cfaad1d2f3498029895e86873c0d8a10
KUCA Brice <kuca.b@sfeir.com>
2024-08-15 17:51:16 +0200

    feat: add Employee Service

--- src/main/java/com/example/api/service/EmployeeService.java
+++ src/main/java/com/example/api/service/EmployeeService.java
@@ -1,9 +1,33 @@
 package com.example.api.service;
 
+import com.example.api.model.Employee;
+import com.example.api.repository.EmployeeRepository;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Data
 @Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Optional<Employee> getEmployee(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Iterable<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        Employee newEmployee = employeeRepository.save(employee);
+        return  newEmployee;
+    }
 }
